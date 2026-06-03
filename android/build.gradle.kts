da6489a24f22cedebebf9509983248eec911445f
mohamad <mohamad@test.com>
2026-06-03 13:58:41 +0330

    force compileSdk 36 globally

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.file.Directory
+
 allprojects {
     repositories {
         google()
@@ -5,16 +7,30 @@ allprojects {
     }
 }
 
+/**
+ * Force all modules to use compileSdk 36
+ * (حل مشکل file_picker و flutter_plugin_android_lifecycle)
+ */
+subprojects {
+    afterEvaluate {
+        extensions.findByName("android")?.let { androidExt ->
+            (androidExt as com.android.build.gradle.BaseExtension).apply {
+                compileSdkVersion(36)
+            }
+        }
+    }
+}
+
 val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+    rootProject.layout.buildDirectory.dir("../../build").get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
